ef56a4cb439333b43b39c1f4b6a73aa658a87256
Yang Zixun <171721832+Senlin001122@users.noreply.github.com>
2024-06-30 21:00:31 +0800

    Create Asterisk Array 3.cpp
    Input: A positive integer n (n<10)
    Output: An array of n rows composed of numbers, where the first row is 1, the second row is 12,..., the nth row is 123... until n, and there is a new line after the last row.

---
+++ Loop Application: Asterisk Array/Asterisk Array 3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int i;
+	cin >> n;
+	for (i = 0; i < n; i++)
+	{
+		for (int j = 1; j <=i+1; j++)
+		{
+			cout << j;
+		}
+		cout << endl;
+	}
+
+		
+	
+}
