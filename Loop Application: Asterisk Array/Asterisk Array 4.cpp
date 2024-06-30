837146c7cf1b422abef67983d4b0dd8449cfb86f
Yang Zixun <171721832+Senlin001122@users.noreply.github.com>
2024-06-30 21:04:37 +0800

    Create Asterisk Array 4.cpp
    Input: n
    Output: n lines, each line has one less asterisk than the left and right sides of the previous line, until the last line has 1 asterisk. Also wrap after the last line. Do not output extra spaces after the last asterisk on each line.

---
+++ Loop Application: Asterisk Array/Asterisk Array 4.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int i;
+	cin >> n;
+	for (i = 0; i < n; i++)
+	{
+		for (int j = 0; j <i; j++)
+		{
+			cout << " ";
+		}
+		for (int k = 0; k < 2 * (n - i) - 1; k++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+		
+	
+}
