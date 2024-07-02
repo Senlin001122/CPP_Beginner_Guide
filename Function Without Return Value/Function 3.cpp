470247e4fd975e0ddd51ae61a3afaf963c7058e1
Yang Zixun <171721832+Senlin001122@users.noreply.github.com>
2024-07-02 19:32:56 +0800

    Create Function 3.cpp
    Use the function to output 10 lines, odd-numbered lines are 5 *, even-numbered lines are 5 #

---
+++ Function Without Return Value/Function 3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void print()
+{
+	cout << "*****" << endl << "#####" << endl << "*****" << endl << "#####" << endl << "*****" << endl << "#####" << endl << "*****" << endl << "#####" << endl << "*****" << endl << "#####";
+
+}
+int main()
+{
+	print();
+	
+}
